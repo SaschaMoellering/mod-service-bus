31b52d643877a45c6ff78b804248cb0f90c779c0
Sascha Möllering <sascha.moellering@zanox.com>
2015-05-10 14:37:23 +0200

    Added test suite and IT

---
+++ src/test/java/com/sm/vertx/mods/ByteArraySerializerIT.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2013 ZANOX AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sm.vertx.mods;
+
+import com.sm.vertx.mods.internal.EventProperties;
+import org.junit.Test;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.testtools.TestVerticle;
+
+import static org.junit.Assert.*;
+import static org.vertx.testtools.VertxAssert.testComplete;
+
+
+/**
+ * Tests mod-service-bus module with byte array serializer configuration.
+ */
+public class ByteArraySerializerIT extends TestVerticle {
+
+    private static final String ADDRESS = "default-address";
+    private static final String MESSAGE = "Test bytes message!";
+
+    @Override
+    public void start() {
+
+        JsonObject config = new JsonObject();
+        config.putString("address", ADDRESS);
+        config.putString("provider.url", "file:///tmp/servicebus.properties");
+
+        System.setProperty("vertx.modulename", "com.sm.vertx~mod-service-bus~1.0.0");
+        container.deployModule(System.getProperty("vertx.modulename"), config, asyncResult -> {
+            assertTrue(asyncResult.succeeded());
+            assertNotNull("DeploymentID should not be null", asyncResult.result());
+            ByteArraySerializerIT.super.start();
+        });
+    }
+
+
+    @Test
+    public void sendMessage() throws Exception {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.putBinary(EventProperties.PAYLOAD, MESSAGE.getBytes());
+
+        Handler<Message<JsonObject>> replyHandler = message -> {
+            assertEquals("ok", message.body().getString("status"));
+            testComplete();
+        };
+        vertx.eventBus().send(ADDRESS, jsonObject, replyHandler);
+    }
+}
---
+++ src/test/java/com/sm/vertx/mods/SuiteIT.java
@@ -0,0 +1,14 @@
+package com.sm.vertx.mods;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ServiceBusMessageProcessorTest.class,
+        ByteArraySerializerIT.class
+})
+public class SuiteIT {
+}
